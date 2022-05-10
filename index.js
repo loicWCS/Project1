8316ce4e871afaa9e973294c0f9f768452c1c7d0
loicWCS <loic.lebon.wcs@gmail.com>
2022-05-11 00:33:32 +0200

    home_375

---
+++ index.js
@@ -0,0 +1,7 @@
+/*Boutton scroll to the top*/
+const myButton = document.querySelector('#topButton')
+function topFunction(){
+    
+  document.documentElement.scrollTop = 0; 
+// renvoie l'Element qui est l'élément racine du document (html)
+}
